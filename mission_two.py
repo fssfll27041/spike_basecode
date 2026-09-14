e08bf8cf3f0cb32b8dafae00a6e89c85004bbfd8
Bolton Robotics FLL Team 27041 <fss.fll.27041@gmail.com>
2026-09-14 14:58:20 -0400

    Week 3: Update Mission two

--- mission_two.py
+++ mission_two.py
@@ -19,6 +19,15 @@ from pybricks.tools import wait, StopWatch
 def mission_two(r: robot):
     print("Running Mission 2")
     # Your code goes here...
+    # r.ram(500, 100,)
+
+    r.robot.straight (670)
+    r.robot.turn (90)
+    r.robot.straight (315)
+    r.robot.turn (-90)
+    r.robot.straight (110)
+    r.lam.run_time (1000, 2000)
+
 
 ################################
 # KEEP THIS AT THE END OF THE FILE
