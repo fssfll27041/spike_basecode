9f4d828c62a4028af5d3485c0f96f92822d083e0
Bolton Robotics FLL Team 27041 <fss.fll.27041@gmail.com>
2026-09-16 16:19:52 -0400

    restore mission five. not tested!

--- mission_five.py
+++ mission_five.py
@@ -20,6 +20,32 @@ def mission_five(r: robot):
     print("Running Mission 5")
     # Your code goes here...
 
+    #the robot drives forward lifts the attachment and then backs.
+    r.robot.straight (750)
+    r.lam.run_time(1000, 1000)
+
+    #Will lower atachment turn and drive forward.
+    r.robot.straight(-200)
+    r.lam.run_time(1000,1000)
+    r.robot.straight(-200)
+
+    #The robot wil get in posistion and drive for to complete the fungie farm mision.
+    r.robot.turn(90)
+    r.robot.straight(200)
+    r.robot.turn(45)
+    r.robot.straight(100)
+    r.robot.turn(90)
+
+    # TEMPORARY CODE
+    r.robot.settings(700, 300, 300, 200)
+    r.robot.straight(200)
+    r.robot.drive(225,45)
+
+
+
+
+
+
 ################################
 # KEEP THIS AT THE END OF THE FILE
 # This redirects to running main.
