8d75a82993237a59213aef655f577bf45a92fad7
Dan Murphy <dan.murphy@littletonrobotics.org>
2026-09-23 16:37:05 -0400

    Starting misssion 6

--- mission_six.py
+++ mission_six.py
@@ -18,7 +18,22 @@ from pybricks.tools import wait, StopWatch
 
 def mission_six(r: robot):
     print("Running Mission 6")
-    # Your code goes here...
+    
+    # Drive forward, stopping next to the tree, then turn right and approach
+    r.robot.straight(710)
+    r.robot.turn(90)
+    r.robot.straight(60)
+
+    # now lift the arm to support the tree
+    r.lam.run_time(500, 500)
+    r.lam.run_time(-500, 500)
+
+    # reverse and go home
+    r.robot.straight(-110)
+    r.robot.turn(-90)
+    r.robot.straight(-710)
+
+
 
 ################################
 # KEEP THIS AT THE END OF THE FILE
