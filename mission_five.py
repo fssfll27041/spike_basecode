bc5f5f125bfedaf03a15e52f2ef7bc5514e167f6
Dan Murphy <dan.murphy@littletonrobotics.org>
2026-09-23 15:52:34 -0400

    mission five's restoration

--- mission_five.py
+++ mission_five.py
@@ -20,27 +20,47 @@ def mission_five(r: robot):
     print("Running Mission 5")
     # Your code goes here...
 
-    #the robot drives forward lifts the attachment and then backs.
-    r.robot.straight (750)
-    r.lam.run_time(1000, 1000)
+################################################################################
+# mission_five.py
+#
+# Description:
+# drive forward, fast up (), drive back while dropping slowly.
+#
+# Author(s): [Your Name(s)]
+# Date: [YYYY-MM-DD]
+# Version: 1.0
+#
+# Dependencies:
+# - robot
+# - pybricks.tools
+#
+################################################################################
+from robot import robot
+from pybricks.tools import wait, StopWatch
 
-    #Will lower atachment turn and drive forward.
-    r.robot.straight(-200)
-    r.lam.run_time(1000,1000)
-    r.robot.straight(-200)
+def mission_five(r: robot):
+    print("Running Mission 5")
+    r.robot.straight(825)
 
-    #The robot wil get in posistion and drive for to complete the fungie farm mision.
-    r.robot.turn(90)
+    r.lam.run_time(400, 1000)
+    r.lam.run_time(-200, 2500)
+    r.robot.straight(-50)
+    r.robot.turn(-90)
+
+    r.robot.straight(260)
+    r.robot.turn(135)
     r.robot.straight(200)
-    r.robot.turn(45)
-    r.robot.straight(100)
+    r.robot.straight(-120)
     r.robot.turn(90)
-
-    # TEMPORARY CODE
-    r.robot.settings(700, 300, 300, 200)
-    r.robot.straight(200)
-    r.robot.drive(225,45)
-
+    r.robot.straight(900)
+    
+################################
+# KEEP THIS AT THE END OF THE FILE
+# This redirects to running main.
+################################
+if __name__ == "__main__":
+    from main import main
+    main()
 
 
 
